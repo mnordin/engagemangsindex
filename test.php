8f7c1574ec66f3ab0a066f81bbfff38030d5dbed
Olle Svensson <bolliolle@gmail.com>
2010-09-04 14:49:49 +0200

    olle

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+$xml = file_get_contents("http://data.riksdagen.se/dokumentstatus/GX02Fi15");
+
+$x = new SimpleXMLElement($xml);
+
+print_r($x->dokaktivitet->aktivitet[0]["namn"]);
+echo "----";
+echo "<pre>";
+print_r($x);
+echo "</pre>";
+
