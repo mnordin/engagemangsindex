9db6567b9db16076322766e82d6b6aa85cf3f7ee
Olle Svensson <bolliolle@gmail.com>
2010-09-05 00:07:25 +0200

    vet egentligen inte vaaad jag gör

---
+++ getdata/getData.php
@@ -0,0 +1,8 @@
+<?php
+
+ $rm = "2009%2F10";
+
+	$url["docList"] = "http://data.riksdagen.se/dokumentlista/?rm=2010&typ=prot&d=&ts=&parti=&iid=&bet=&org=&kat=&sz=100&sort=c&utformat=xml&termlista=";
+	
+
+print_r($data);
--- getdata/getDataToDb.php
+++ getdata/getDataToDb.php
@@ -1,16 +1,28 @@
 <?php
-//include_once "../includeAllFunctions.php";
+include_once "../bootstrap.php";
 $i = 1;
 do {
 	$docList = simplexml_load_file("http://data.riksdagen.se/dokumentlista/?rm=2009%2F10&typ=mot&utformat=xml&p=".$i);
 	$docStatus = simplexml_load_file($docList->dokument->dokumentstatus_url_xml);
 	usleep(500);
+	//fält
 	echo "<pre>";
-	print_r($docList);
-	echo "</pre>";
+	 $dok_id = $docStatus->dokument->dok_id;
+	 $titel=$docStatus->dokument->titel;
+	 $antal_intressenter = getNumberOfIntressenter($docStatus);
+	 $organ = $docStatus->dokument->organ; 
+	 $datum = $docStatus->dokument->datum;
+	 $rm = $docStatus->dokument->rm;
+	 $subtyp = $docStatus->dokument->subtyp;
+	 $hangar_id =$docStatus->dokument->hangar_id;
+	mysql_query("INSERT INTO motion 
+		(dok_id , titel , antal_intressenter , organ , datum , rm , hangar_id )
+		VALUES ( $dok_id ,$titel,$antal_intressenter,$organ,$datum,$rm,$hangar_id");
+	echo $dok_id . ":".$titel. ":".$antal_intressenter. ":".$organ. ":".$datum. ":".$rm. ":".$hangar_id. ":";
 	echo "NÄSTA POST". $docList->attributes()->nasta_sida;
+	echo "</pre>";
 	//spara till db
 	$i++;
 }
-	while ($i<=3);
+	while ($i<=10);
 ?>
