57125320ca04a83738f8338416794e6681b4d6aa
Olle Svensson <bolliolle@gmail.com>
2010-09-04 19:09:18 +0200

    loading things to db

--- getdata/getDataToDb.php
+++ getdata/getDataToDb.php
@@ -1,15 +1,16 @@
 <?php
-include_once "../includeAllFunctions.php";
-
-$docList = simplexml_load_file("http://data.riksdagen.se/dokumentlista/?rm=2009%2F10&typ=mot&utformat=xml&p=3");
-echo "<pre>";
-print_r($docList);
-echo $_SERVER['PHP_SELF'];
-echo "</pre>";
-
-$docStatus = simplexml_load_file($docList->dokument->dokumentstatus_url_xml);
-echo "<pre>";
-echo getNumberOfIntressenter($docStatus);
-echo "</pre>";
-
+//include_once "../includeAllFunctions.php";
+$i = 1;
+do {
+	$docList = simplexml_load_file("http://data.riksdagen.se/dokumentlista/?rm=2009%2F10&typ=mot&utformat=xml&p=".$i);
+	$docStatus = simplexml_load_file($docList->dokument->dokumentstatus_url_xml);
+	usleep(500);
+	echo "<pre>";
+	print_r($docList);
+	echo "</pre>";
+	echo "N€STA POST". $docList->attributes()->nasta_sida;
+	//spara till db
+	$i++;
+}
+	while ($i<=3);
 ?>
