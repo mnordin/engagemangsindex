8bf9eb275647988518c45154d0eaf2f5ff535f84
antt <antt@antte-laptop.(none)>
2010-09-04 22:16:15 -0500

    jgcharts testing

--- index.php
+++ index.php
@@ -16,6 +16,21 @@
 		<title>Engagemangsindex</title>
 		<meta name="description" content="">
 		<link rel="stylesheet" href="stylesheets/structure.css" media="all">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+		<script src="js/jgcharts.pack.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				var api = new jGCharts.Api(); 
+				jQuery('<img>') 
+				.attr('src', api.make({
+					data : [[<?=$_GET['1_1']?>], [<?=$_GET['2_1']?>], [<?=$_GET['3_1']?>]],
+					type : 'p',
+					size : '400x400',
+					legend : ['hej', 'nej', 'tjej']
+				})) 
+				.appendTo("#bar1");
+			});
+		</script>
 	</head>
 	<body>
 		<section>
@@ -27,6 +42,24 @@
 					</ul>
 				</nav>
 			</header>
+			
+			<div id="bar1"></div>
+			
+			<form method="GET" action="">
+				<input name="1_1" type="text" value="<?=$_GET['1_1']?>"></input>
+				<input name="1_2" type="text" value="<?=$_GET['1_2']?>"></input>
+				<input name="1_3" type="text" value="<?=$_GET['1_3']?>"></input>
+				<input name="2_1" type="text" value="<?=$_GET['2_1']?>"></input>
+				<input name="2_2" type="text" value="<?=$_GET['2_2']?>"></input>
+				<input name="2_3" type="text" value="<?=$_GET['2_3']?>"></input>
+				<input name="3_1" type="text" value="<?=$_GET['3_1']?>"></input>
+				<input name="3_2" type="text" value="<?=$_GET['3_2']?>"></input>
+				<input name="3_3" type="text" value="<?=$_GET['3_3']?>"></input>
+				<input type="submit"></input>
+			</form>
+			
+			<br />
+			
 				<table>
 					<thead>
 						<tr>
